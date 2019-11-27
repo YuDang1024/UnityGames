75a66cce598cb824bcf2c88bf90ed685fe9c3323
dangyu <374489401@qq.com>
2019-11-27 01:14:44 -0800

    简单的FPS射击游戏

---
+++ FPSSample/Assets/Scripts/Fire.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    // 开火动画
+    public ParticleSystem fireParticleSystem;
+    // 开火音效
+    public AudioSource fireAudioSource;
+    // 击中生成的碎片
+    public GameObject piece;
+    // 发出射线的位置
+    // 因为射线都是从眼睛的位置发射，然后方向是眼睛局部坐标的正前方，
+    public Transform eye;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shooting(); 
+        }
+    }
+
+    void Shooting()
+    {
+        // 播放开火动画
+        this.fireParticleSystem.Play();
+        // 播放开火音乐
+        // 使用这个函数去播放的话，会更急促一些
+        this.fireAudioSource.PlayOneShot(this.fireAudioSource.clip);
+
+        // 检测射线碰撞的点，然后在碰撞的点的位置生成粒子特效
+        RaycastHit hit;
+        // 射线的起，射线的方向，击中的物体，射线的最远距离
+        if (Physics.Raycast(this.eye.position,this.eye.forward,out hit, 100))
+        {
+            //击中之后，根据击中点的坐标，生成预制体
+            MakePrefab(hit.point);
+             
+        }
+    }
+
+    void MakePrefab(Vector3 position)
+    {
+        for(int i = 0; i<5; i++)
+        {
+            GameObject p = Instantiate(piece);
+            p.transform.position = position;
+            p.GetComponent<Rigidbody>().AddForce(this.eye.forward * 100f);
+            // 1秒之后消失
+            Destroy(p, 1);
+        }
+    }
+}
---
+++ FPSSample/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // 行走的速度
+    public AudioSource walkAudioSource;
+    public float walkSpeed;
+    public Transform eye;
+    public Transform rightHand;
+
+    // 绕Y轴旋转
+    // 左右的旋转是按照Y轴旋转的
+    float rota_y;
+    // 绕X轴旋转
+    // 上下的移动是绕X轴旋转
+    float rota_x;
+
+    //鼠标移动的像素的距离对应的角度的比例，俗称：鼠标灵敏度
+    public float sensitivity = 5.0f; // 移动1，对应5度
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Walk())
+        {
+            walkAudioSource.Play(); 
+        }
+
+        LookAround();
+    }
+
+    bool Walk()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(0,0,walkSpeed * Time.deltaTime);
+            return true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-walkSpeed * Time.deltaTime, 0, 0);
+            return true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(0, 0, -walkSpeed * Time.deltaTime);
+            return true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(walkSpeed * Time.deltaTime, 0, 0);
+            return true;
+        }
+        return false;
+    }
+
+    void LookAround() 
+    {
+        // 左右移动，绕Y
+        rota_y += (Input.GetAxis("Mouse X") * sensitivity);
+        rota_y = Mathf.Clamp(rota_y,-360,360);
+
+        Vector3 rotaY = this.transform.localEulerAngles;
+        rotaY.y = rota_y;
+        this.transform.localEulerAngles = rotaY;
+
+        // 上下移动,绕X
+        rota_x += (Input.GetAxis("Mouse Y") * sensitivity);
+        rota_x = Mathf.Clamp(rota_x, -45, 45);
+        Vector3 rotaX = this.eye.localEulerAngles;
+        rotaX.x = -rota_x;
+        this.eye.localEulerAngles = rotaX;
+
+        // 握枪的手也要上下摇摆
+        this.rightHand.localEulerAngles = rotaX;
+
+    }
+}
